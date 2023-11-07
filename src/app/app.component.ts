e5fbaa623b20fe7ca2732d71f1724ed94eb3d608
AnkitBaranwal098 <ankitbaranwal1998@gmail.com>
2023-11-07 21:34:51 +0530

    Sending a GET request demp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,24 +9,33 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  ngOnInit() {}
+  ngOnInit() { 
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     // console.log(postData);
-    this.http.post('https://ng-complete-guide-b5e4c-default-rtdb.firebaseio.com/posts.json',postData).subscribe((responseData)=>{
+    this.http.post('https://ng-complete-guide-b5e4c-default-rtdb.firebaseio.com/posts.json', postData).subscribe((responseData) => {
       console.log(responseData)
     })
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts()
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts(){
+    this.http.get('https://ng-complete-guide-b5e4c-default-rtdb.firebaseio.com/posts.json').subscribe((posts)=>{
+      console.log(posts)
+    })
   }
 }
 
